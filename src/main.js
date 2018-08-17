ca3f69225853752e0476b0cce773ff020c34d63f
Xing <sky00000kimo@gmail.com>
2018-08-17 06:57:06 +0000

    fix main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
+import App from './App'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+Vue.use(router)
+
+
 
 new Vue({
   store,
-  router
+  router,
+  render: h => h(App)
 }).$mount('#app')
